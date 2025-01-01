9e7533fbf88ab50f6c3f1a01830e22c61220ffe6
unknown <manafikhiamjad@gmail.com>
2025-01-01 23:27:15 +0300

    empt form validation

--- src/chef/chefmain.jsx
+++ src/chef/chefmain.jsx
@@ -11,13 +11,15 @@ export default function ChefMain (props) {
     
     const [formClass,setFormClass]=React.useState();
     const [ingredients,setIngredients]=React.useState([]);
+    const [unvalid,setUnvalid]=React.useState("e.g. flour");
     
     
     function handleNewingredient(formData){
         let ingredientName=formData.get("ingredient");
-        if(ingredientName.length===0){setFormClass({borderColor:"red"}) }
+        if(ingredientName.length===0||ingredientName==="you can't submit an empty label"){setFormClass({borderColor:"rgb(200, 0, 0)"}) ;setUnvalid("you can't submit an empty label"); }
         else {
             setFormClass({borderColor:"black"})
+            setUnvalid("e.g. flour");
         setIngredients(() => [...ingredients,ingredientName]);
         props.handle(ingredients.length+1/*,getRecipeFromMistral(ingredients)*/)
         }
@@ -35,7 +37,7 @@ export default function ChefMain (props) {
                     style={formClass}
                     aria-label="Add ingredient"
                     type="text"
-                    placeholder="e.g. flour"
+                    placeholder={unvalid}
                     name="ingredient"
                      pattern=".*\S.*"
                      
